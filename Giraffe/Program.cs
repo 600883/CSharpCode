1f5e2d78092de80cc5f18133ac7b30667227425d
sivertseter <sivertseter@yahoo.com>
2023-02-28 12:43:43 +0100

    Mitt første c# prosjekt

---
+++ Giraffe/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string color, pluralNoun, celebrity;
+
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+
+            Console.Write("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter a celebrity: ");
+            celebrity = Console.ReadLine();
+
+
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine( pluralNoun + " are red");
+            Console.WriteLine("I love " + celebrity);
+
+
+            Console.ReadLine();
+           
+        }
+    }
+
+}
